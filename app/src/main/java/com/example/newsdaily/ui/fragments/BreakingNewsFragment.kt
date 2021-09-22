760e125c88007c905236464473944802e2085c3c
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-09-22 21:20:41 +0530

    News Daily App - Handling Breaking News Response

--- app/src/main/java/com/example/newsdaily/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsdaily/ui/fragments/BreakingNewsFragment.kt
@@ -1,19 +1,67 @@
 package com.example.newsdaily.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsdaily.R
+import com.example.newsdaily.adapters.NewsAdapter
 import com.example.newsdaily.ui.NewsActivity
 import com.example.newsdaily.ui.NewsViewModel
+import com.example.newsdaily.util.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
     lateinit var viewModel: NewsViewModel
+
+    lateinit var newsAdapter: NewsAdapter
+    val TAG = "BreakingNewsFragment"
     //private val viewModel : NewsViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
 
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error ->{
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occurred: $message")
+                        Toast.makeText(activity, "An error occurred: $message", Toast.LENGTH_LONG).show()
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+
+        })
+
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
