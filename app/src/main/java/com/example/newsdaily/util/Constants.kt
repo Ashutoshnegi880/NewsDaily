1686887d69f4531949b13ec44696e7313cfd2e8f
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-09-29 11:59:14 +0530

    News Daily App - Handling Searching News

--- app/src/main/java/com/example/newsdaily/util/Constants.kt
+++ app/src/main/java/com/example/newsdaily/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "5f8540d95b02449c8c3ee21a6c2a7ea3"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
