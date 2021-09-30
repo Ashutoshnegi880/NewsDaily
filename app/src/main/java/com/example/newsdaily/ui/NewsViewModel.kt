ef574796e4ef5f76c42f0699130f34336000ce8e
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-09-30 11:25:35 +0530

    News Daily App - Adding and Deleting Articles to our Room Database

--- app/src/main/java/com/example/newsdaily/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsdaily/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.newsdaily.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newsdaily.models.Article
 import com.example.newsdaily.models.NewsResponse
 import com.example.newsdaily.repository.NewsRepository
 import com.example.newsdaily.util.Resource
@@ -55,6 +56,15 @@ class NewsViewModel(
             }
         }
         return Resource.Error(response.message())
+    }
 
+    fun saveArticle(article: Article) = viewModelScope.launch{
+        newsRepository.upsert(article)
+    }
+
+    fun getSavedNews() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
     }
 }
