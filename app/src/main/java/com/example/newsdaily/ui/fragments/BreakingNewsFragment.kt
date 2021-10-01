d75b0634553fddc4a81a85d5dee11889208965ea
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-10-01 11:39:39 +0530

    News Daily App - Handling Pagination with Retrofit

--- app/src/main/java/com/example/newsdaily/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsdaily/ui/fragments/BreakingNewsFragment.kt
@@ -3,15 +3,18 @@ package com.example.newsdaily.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsdaily.R
 import com.example.newsdaily.adapters.NewsAdapter
 import com.example.newsdaily.ui.NewsActivity
 import com.example.newsdaily.ui.NewsViewModel
+import com.example.newsdaily.util.Constants.Companion.QUERY_PAGE_SIZE
 import com.example.newsdaily.util.Resource
 import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
@@ -41,7 +44,12 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let { newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles)
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.breakingNewsPage == totalPages
+                        if (isLastPage){
+                            rvBreakingNews.setPadding(0,0,0,0)
+                        }
                     }
                 }
                 is Resource.Error ->{
@@ -62,10 +70,45 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
 
     private fun hideProgressBar() {
         paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
     }
 
     private fun showProgressBar() {
         paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >=0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning
+                    && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate){
+                viewModel.getBreakingNews("in")
+                isScrolling = false
+            }
+        }
     }
 
     private fun setupRecyclerView(){
@@ -73,6 +116,8 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
         rvBreakingNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+
+            addOnScrollListener(this@BreakingNewsFragment.scrollListener)
         }
     }
 }
--- app/src/main/java/com/example/newsdaily/util/Constants.kt
+++ app/src/main/java/com/example/newsdaily/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "5f8540d95b02449c8c3ee21a6c2a7ea3"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
